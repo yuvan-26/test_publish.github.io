86d2ec0dc38e04b5c39727cfb7f7656a1a05c729
Hexuan Wang <hwan0119@student.monash.edu>
2024-05-14 14:20:12 +1000

    i3 o2

--- disease_hub.js
+++ disease_hub.js
@@ -3,6 +3,7 @@ document.addEventListener('DOMContentLoaded', function () {
         .then(response => response.json())
         .then(data => {
             populateLists(data);
+            displayRandomDiseases(data);
         });
 
     function populateLists(data) {
@@ -29,6 +30,39 @@ document.addEventListener('DOMContentLoaded', function () {
         setupToggle('viralBtn', 'viralList', [bacterialList, fungalList]);
     }
 
+    function displayRandomDiseases(data) {
+
+        const randomDiseases = chooseRandom(data, 4); // Choose 4 random diseases
+        const galleryDiv = document.getElementById('diseaseInfo');
+        galleryDiv.innerHTML = ''; // Clear previous contents if any
+
+        randomDiseases.forEach(disease => {
+            const imgContainer = document.createElement('div');
+            imgContainer.innerHTML =
+                `
+                                    <div class="disease-card">
+                                    <div class="image-container-disease">
+                                        <img src="${disease['Image_url']}" alt="${disease['Disease_name']} Image" class="plant-image-modeling">
+                                    </div>
+                                    <div style="display: flex; justify-content: center; align-items: center; width: 100%; height: 30%; background-color: #403323;">
+                                        <div style="width: 90%; font-size: 20px; font-family: 'Jost'; color: #FFFFFF; text-align: center;">
+                                            ${disease['Disease_name']}
+                                        </div>
+                                    </div>
+                                    </div>
+                                    `
+
+            galleryDiv.appendChild(imgContainer);
+
+            imgContainer.addEventListener('click', () => displayDiseaseInfo(disease));
+        });
+    }
+
+    function chooseRandom(data, num) {
+        const shuffled = data.sort(() => 0.5 - Math.random());
+        return shuffled.slice(0, num);
+    }
+
 
     function setupToggle(buttonId, listId, otherLists) {
         const button = document.getElementById(buttonId);
@@ -50,7 +84,7 @@ document.addEventListener('DOMContentLoaded', function () {
         infoDiv.innerHTML = `
                             <div style="display: flex; font-family: 'Jost'; width:100%">
                                 <div style="width: 55%; gap: 30px;">
-                                    <div style="font-size: 30px; font-weight: 400px">${disease.Disease_name}</div>
+                                    <div style="font-size: 30px; font-weight: 400px; margin-bottom: 20px">${disease.Disease_name}</div>
                                     <div><strong>Description:</strong> ${disease.Description}</div>
                                     <div><strong>Causes:</strong> ${disease.Causes}</div>
                                     <div><strong>Symptoms:</strong> ${disease.Symptoms}</div>
@@ -58,7 +92,7 @@ document.addEventListener('DOMContentLoaded', function () {
                                 </div>
                                 <div style="width: 40%;">
                                 <div style="overflow: hidden; width: 350px; height: 350px">
-                                    <image src=${disease.Image_url}>
+                                    <img src=${disease.Image_url}>
                                 </div>
                                 </div>
                             </div>
